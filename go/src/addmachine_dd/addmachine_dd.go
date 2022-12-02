873f930ca93315eb53af3cd6b3ccb97458f3b3df
181803919 <181803919@qq.com>
2022-12-02 16:55:14 +0800

    addmachine重命名成addmachine_dd

---
+++ go/src/addmachine_dd/addmachine_dd.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fflog"
+	"net/http"
+)
+
+func modifyMachine(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	if r.Method == "POST" {
+		fflog.FFDebug("after decrypt msg: POST")
+	}
+}
+
+func main() {
+	//	ffdaemon.Daemon()
+	fflog.Open()
+	defer fflog.Close()
+
+	fflog.FFDebug("Listen on Port 16669")
+	http.HandleFunc("/modifyMachine", modifyMachine)
+
+	err := http.ListenAndServe(":16669", nil)
+	if err != nil {
+		fflog.FFError("Listen 16669 fail" + err.Error())
+		return
+	}
+}
